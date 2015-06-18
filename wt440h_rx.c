6f04ce8549db5ebaca1565fa285ad54823849eac
Gergely Budai <gbudai76@gmail.com>
2015-06-18 17:51:41 +0200

    Added GPL license text.

--- wt440h_rx.c
+++ wt440h_rx.c
@@ -1,10 +1,24 @@
 /***********************************************************************************************************************
  *
- * WT440H Receiver for Raspberry Pi
+ * WT440H Receiver kernel module for Raspberry Pi
  *
- * (C) 2015 Gergely Budai
+ * Copyright (C) 2015 Gergely Budai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  **********************************************************************************************************************/
+
 #define DRIVER_NAME "wt440h"
 #define DESCRIPTION "WT440H - Wireless Temperature and Humidity sensor receiver driver"
 #define AUTHOR      "Gergely Budai"
